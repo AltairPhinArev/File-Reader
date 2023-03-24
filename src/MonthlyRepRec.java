c876fe52c8eb96fdd1fdbb555729ef4a2b79c8d5
Andrey Panasiuk <113471380+AltairPhinArev@users.noreply.github.com>
2023-03-24 09:07:48 +0100

    Update MonthlyRepRec.java

--- src/MonthlyRepRec.java
+++ src/MonthlyRepRec.java
@@ -10,7 +10,7 @@ public class MonthlyRepRec {
     Integer suMofOne;
     public MonthlyRepRec(String[] lineContecst){
         itemName = lineContecst[0];
-        isExpenses =Boolean.parseBoolean( lineContecst[1]);
+        isExpenses = Boolean.parseBoolean( lineContecst[1]);
         quantitity = Integer.parseInt(lineContecst[2]);
         suMofOne = Integer.parseInt(lineContecst[3]);
     }
