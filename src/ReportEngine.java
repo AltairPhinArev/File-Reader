7bce0cb72803f7aedbc9847c241300aa8244e1de
Andrey Panasiuk <113471380+AltairPhinArev@users.noreply.github.com>
2023-06-20 16:14:20 +0300

    Update ReportEngine.java

--- src/ReportEngine.java
+++ src/ReportEngine.java
@@ -1,5 +1,10 @@
 import  java.util.ArrayList;
 
+/*
+    @author Andrey Panasiuk
+    Git @AltairPhinArev
+ */
+
 public class ReportEngine {
     OneSideOfMonth oneSideOfMonth = new OneSideOfMonth();
     OneSideOfYear oneSideOfYear = new OneSideOfYear();
